2a597f8367435476f5c9911fbaf6dc0b8cd0b369
simone-art <simoneperiodista3@gmail.com>
2020-04-15 18:08:57 -0300

    Exercicio sobre relacionamento de objetos

---
+++ Carro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Framework
+{
+    public class Carro
+    {
+        // O key sinaliza que o id é primary key e autoincremente en la base de dados//
+        [Key()]
+        public int Id { get; set; }
+
+        [ForeignKey("Modelo")]
+        public int ModeloId { get; set; }
+
+        public string Nome { get; set;}
+
+        public int AnoDoCarro { get; set; }
+
+    }
+}
